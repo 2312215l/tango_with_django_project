fd79d845e4f13728413d39f3afb936411816dabe
Yada Lawhavinit <2312215l@student.gla.ac.uk>
2018-01-22 21:11:19 +0000

    Update Ch.5

--- populate_rango.py
+++ populate_rango.py
@@ -36,9 +36,10 @@ def populate():
         {"title": "Flask",
          "url": "http://www.flask.pocoo.org"}]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages}}
+    cats = {"Python": {"pages": python_pages,"views": 128, "likes": 64},
+            "Django": {"pages": django_pages,"views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages,"views": 32, "likes": 16}}
+
 
 
     #If you want to add more catergories or pages,
@@ -65,8 +66,10 @@ def add_page(cat,title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name,views=0,likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
 
